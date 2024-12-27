fe61d96dff89eb26543fd322f9634350feabe871
iana01 <iana@digidot.eu>
2024-12-27 15:32:11 +0100

    req logout

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,13 +1,17 @@
+import client from "@/lib/apolloClient";
 import { FC, PropsWithChildren } from "react";
 import { Navigation } from "../navigation";
+import { ApolloProvider } from "@apollo/client";
 
 export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex h-full flex-col items-center justify-center">
-      <Navigation />
-      <div className="w-full flex-1 overflow-auto px-4">
-        <div className="py-8">{children}</div>
+    <ApolloProvider client={client}>
+      <div className="flex h-full flex-col items-center justify-center">
+        <Navigation />
+        <div className="w-full flex-1 overflow-auto px-4">
+          <div className="py-8">{children}</div>
+        </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 };
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,20 +1,43 @@
+"use client";
+
 import { LogIn } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+import { gql, useMutation, useLazyQuery } from "@apollo/client";
+import { useRouter } from "next/navigation";
+
+const LOGOUT_QUERY = gql`
+  query LogoutUser {
+    logoutUser
+  }
+`;
 
 export const Navigation = () => {
+  const router = useRouter();
+
+  const [logoutUser, {loading, error}] = useLazyQuery(LOGOUT_QUERY)
+
+  const handleLogout = async () => {
+    try {
+      await logoutUser();
+      console.log("Выход выполнен успешно");
+    } catch (err) {
+      console.error("Ошибка во время выхода:", err);
+    }
+  };
+
   return (
     <div className="right-0 top-0 z-10 flex w-full items-center justify-end p-3 sm:fixed">
-      <Link
-        href="/dashboard"
+      <button
+        onClick={handleLogout}
         className={buttonVariants({
           variant: "outline",
           size: "sm",
         })}
       >
-        Войти
+        Выйти
         <LogIn />
-      </Link>
+      </button>
     </div>
   );
 };
